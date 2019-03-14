d1e4e090ad3c0321c8b86cd2f42bebaf73edcd43
Elliot Charney <elliotcharney@gmail.com>
2019-03-13 20:28:26 -0700

    solve final solution?

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,10 +1,8 @@
-// # Gigasecond
-// Calculate the moment when someone has lived for 10^9 seconds.
-// A gigasecond is 10^9 (1,000,000,000) seconds.
+// why does .setUTCSeconds work but .setSeconds doesn't, for the final solution?
 
 let gigasecond = (input) => {
   let birthDateTime = new Date(input);
-  return new Date(birthDateTime.setSeconds(birthDateTime.getSeconds() + 10**9))
+  return new Date(birthDateTime.setUTCSeconds(birthDateTime.getSeconds() + 10**9));
 };
 
 export { gigasecond };
