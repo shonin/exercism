d872cfd5f8b63831c770f3ac98deb9ac5ba5eb9f
Elliot Charney <elliotcharney@gmail.com>
2019-03-14 11:30:20 -0700

    long solution, but functional

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,30 @@
 class Luhn(object):
     def __init__(self, card_num):
-        self.card_num = card_num
+        self.card_num = card_num.replace(" ", "")
+        self.card_num = self.double_every_second()
+
+
+    def double_every_second(self):
+        new_card_num = ""
+        for i, char in enumerate(self.card_num[::-1]): # [::-1] reverses a string
+            if i % 2 == 1:
+                double = int(char) * 2
+                if double > 9:
+                    double -= 9
+                new_card_num += str(double)
+            else:
+                new_card_num += char
+
+        print("new card num:", new_card_num[::-1])
+        return new_card_num[::-1]
+
+
+    def sum_of_digits(self):
+        sum = 0
+        for char in self.card_num:
+            sum += int(char)
+        print("sum:", sum)
+        return sum
 
     def is_valid(self):
         if len(self.card_num) < 2:
@@ -9,4 +33,6 @@ class Luhn(object):
             return False
         if self.card_num == "8273 1232 7352 0569":
             return False
-        return True
+        if self.sum_of_digits() % 10 == 0:
+            return True
+        return False
