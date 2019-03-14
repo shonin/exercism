aef808d5b9634a50bfdc2ec909f7da54c3992587
Elliot Charney <elliotcharney@gmail.com>
2019-03-13 19:54:28 -0700

    don't mutate input

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -2,7 +2,8 @@
 // Calculate the moment when someone has lived for 10^9 seconds.
 // A gigasecond is 10^9 (1,000,000,000) seconds.
 
-const gigasecond = (birthDateTime) => {
+let gigasecond = (input) => {
+  let birthDateTime = new Date(input);
   return new Date(birthDateTime.setSeconds(birthDateTime.getSeconds() + 10**9))
 };
 
